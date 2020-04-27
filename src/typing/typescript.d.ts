be5bb21192585ef5c68d36b47ebf0dc5430bcd67
kingwl <kingwenlu@gmail.com>
2020-04-27 22:44:41 +0800

    use text changes rather than transformer

---
+++ src/typing/typescript.d.ts
@@ -0,0 +1,26 @@
+import { Program } from 'typescript'
+
+declare module "typescript" {
+    export namespace formatting {
+        export interface FormatContext {
+            readonly options: FormatCodeSettings;
+            readonly getRules: unknown;
+        }
+
+        function getFormatContext(options: FormatCodeSettings): FormatContext
+    }
+
+    export namespace textChanges {
+        export interface TextChangesContext {
+            host: LanguageServiceHost;
+            formatContext: formatting.FormatContext;
+            preferences: UserPreferences;
+        }
+    
+        export class ChangeTracker {
+            public static with(context: TextChangesContext, cb: (tracker: ChangeTracker) => void): FileTextChanges[];
+        }
+    }
+
+    export function getDefaultFormatCodeSettings(newLineCharacter?: string): FormatCodeSettings;
+}
