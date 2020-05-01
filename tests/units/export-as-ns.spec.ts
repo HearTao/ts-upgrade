f47653d47a54eb186ddf52019d9692641c968716
stkevintan <stkevintan@zju.edu.cn>
2020-05-01 10:46:16 +0800

    feat: add test case and opt upgrade

--- tests/units/export-as-ns.spec.ts
+++ tests/units/export-as-ns.spec.ts
@@ -4,7 +4,7 @@ import { prettierEqTo } from '../utils';
 describe('export * as ns ugrade', () => {
     const version = TypeScriptVersion.V3_8;
 
-    it('should work with simple import', () => {
+    it('should work with simple case', () => {
         const code = `import * as A from "./a.js";\nexport { A };`;
         const after = `export * as A from "./a.js";`;
         prettierEqTo(upgrade(code, version), after);
@@ -18,13 +18,19 @@ describe('export * as ns ugrade', () => {
 
     it('should work with multiple named exports', () => {
         const code = `import * as A from './a.js';\n export { A, B as C, D };`;
-        const after = `export { B as C, D };\nexport * as A from './a.js';`;
+        const after = `export * as A from './a.js';\nexport { B as C, D };`;
         prettierEqTo(upgrade(code, version), after);
     });
 
-    it('should work with named export with property', () => {
+    it('should work with property exports', () => {
         const code = `import * as A from './a.js';\nexport { A as B };`;
         const after = `export * as B from './a.js';`;
+        prettierEqTo(upgrade(code, version), after);
+    });
+
+    it('should work with multiple case', () => {
+        const code = `import * as A from './a.js';\nimport * as B from './b.js';\nexport {A, B};`;
+        const after = `export * as A from './a.js';\nexport * as B from './b.js';`;
         prettierEqTo(upgrade(code, version), after);
     });
 });
